b3c80f39da441b5612731fa39fdb0703c5651c4f
Sjoerd <sjoerdmoret6@gmail.com>
2016-11-09 14:35:56 +0100

    first commit

---
+++ Assets/Scripts/PinchZoom.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PinchZoom : MonoBehaviour {
+    public GameObject stadium;
+    private Vector3 position;
+
+    void Start()
+    {
+        //transform.position = 
+        //offset = transform.position - player.transform.position;
+    }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.touchCount == 1)
+        {
+          //  EditorUtility.DisplayDialog("title", "gelukt", "oke");
+            Vector3 movement = new Vector3(1.0f, 0.0f, 0.0f);
+
+            rb.AddForce(movement * speed);
+        }
+        if (Input.touchCount == 2)
+        {
+            //  EditorUtility.DisplayDialog("title", "gelukt", "oke");
+            Vector3 movement = new Vector3(-1.0f, 0.0f, 0.0f);
+
+            rb.AddForce(movement * speed);
+        }
+    }
+
+    void FixedUpdate(){
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+}
